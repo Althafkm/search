dd35ff5085f1e17e88e795e673b72767e7a30e1f
k mohammed althaf <kmalthaf79@gmail.com>
2022-02-15 13:56:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Data from './Data.json'
+import {useState} from 'react'
 
 function App() {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  
+
+ 
+  const submit=(e)=>{
+    e.preventDefault()
+    const newFilter = Data.filter((value) => {
+      return value.title.toLowerCase().includes(wordEntered.toLowerCase());
+    });
+    setFilteredData(newFilter)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <div className='header-part'>
+          <h1>Books Gallery</h1>
+        <form>
+          <input type="text" style={{margin:'10px'}} value={wordEntered} onChange={(e) =>{
+            setWordEntered(e.target.value)}} />
+          <button type="submit" onClick={submit}> search</button>
+        </form>
+      </div>
+      <div>
+      <div>
+           <div className='flexContainer'>
+            <div className='heads'>
+            <h3>Book Name</h3>
+            </div>
+            <div className='heads'>
+            <h3>Author</h3>
+            </div>
+           </div>
+         {
+            filteredData.map((value) => {
+  
+          return (
+                <div className='flexContainer'>
+                
+                <div className='heads'>
+                <a className='layer' href='https://www.w3schools.com'>{value.title} </a>
+                </div>
+                <div className='heads'>
+                <p>{value.author}</p>
+                </div>
+              </div>
+                  );
+          })}
+        
+
+      </div>
+      </div>
+
+
     </div>
   );
 }
-
 export default App;
